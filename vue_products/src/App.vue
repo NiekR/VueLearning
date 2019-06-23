ac7d05c4f9b14ded9955467f442737d8218d7f20
LAPTOP-LK4OIR7G\niek_ <niek_riggeling@hotmail.com>
2019-06-23 19:54:25 +0200

    start small course vue.js

---
+++ vue_products/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Product></Product>
+  </div>
+</template>
+
+<script>
+import Product from './components/Product.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Product
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
